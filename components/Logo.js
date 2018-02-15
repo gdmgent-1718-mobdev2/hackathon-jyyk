6f1c11537d52e797c785680dfba362657a85286a
Kevin Leemans <kevileem2@student.arteveldehs.be>
2018-02-15 21:18:02 +0100

    added logo component and cleaned up initialpage

---
+++ components/Logo.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, StyleSheet, Image } from 'react-native';
+
+export class Logo extends React.Component {
+  render(){
+    return(
+        <Image
+            style={styles.image}
+            source={require('../assets/images/logo.png')}
+        />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  image: {
+    flex: 0,
+    width: 250,
+    height: 170
+  }
+});
